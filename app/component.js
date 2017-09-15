38a321fe9fc0986b9091c84a5d6bb269ef177305
Nigel Wade <nigel.wade@zonal.co.uk>
2017-09-15 13:21:59 +0100

    Adds the actual speech synthesis basic code

--- app/component.js
+++ app/component.js
@@ -1,6 +1,24 @@
-export default (text = 'Hello world') => {
+export default () => {
     const element = document.createElement('div');
-    element.className = 'pure-button';
-    element.innerHTML = text;
+    element.className = 'synth-form';
+    const form = document.createElement('form');
+    const txtInput = document.createElement('input');
+    txtInput.classList.add('txt');
+    const voiceSelect = document.createElement('select');
+    const pitchValue = document.createElement('input');
+    pitchValue.classList.add('pitch-value');
+    pitchValue.type = 'number';
+    const rateValue = document.createElement('input');
+    rateValue.classList.add('rate-value');
+    rateValue.type = 'number';
+    const submit = document.createElement('input');
+    submit.type = 'submit';
+    submit.value = 'Submit';
+    form.appendChild(txtInput);
+    form.appendChild(voiceSelect);
+    form.appendChild(pitchValue);
+    form.appendChild(rateValue);
+    form.appendChild(submit);
+    element.appendChild(form);
     return element;
 }
--- app/index.js
+++ app/index.js
@@ -3,3 +3,53 @@ import 'purecss';
 import './main.css';
 
 document.body.appendChild(component());
+
+var synth = window.speechSynthesis;
+
+var inputForm = document.querySelector('form');
+var inputTxt = document.querySelector('.txt');
+var voiceSelect = document.querySelector('select');
+
+var pitchValue = document.querySelector('.pitch-value');
+var rateValue = document.querySelector('.rate-value');
+
+var voices = [];
+
+const populateVoiceList = () => {
+    voices = synth.getVoices();
+
+    for (let i = 0; i < voices.length; i++) {
+        var option = document.createElement('option');
+        option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+
+        if (voices[i].default) {
+            option.textContent += ' -- DEFAULT';
+        }
+
+        option.setAttribute('data-lang', voices[i].lang);
+        option.setAttribute('data-name', voices[i].name);
+        voiceSelect.appendChild(option);
+    }
+}
+
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+inputForm.onsubmit = function (event) {
+    event.preventDefault();
+
+    var utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+    var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    for (let i = 0; i < voices.length; i++) {
+        if (voices[i].name === selectedOption) {
+            utterThis.voice = voices[i];
+        }
+    }
+    utterThis.pitch = pitchValue.value;
+    utterThis.rate = rateValue.value;
+    synth.speak(utterThis);
+
+    inputTxt.blur();
+}
